c91d63647de8d04b07070b4be3e54e2f45887ca9
Travis Little <kingtrav@gmail.com>
2019-04-23 19:24:51 -0600

    mvp and stretch met. final commit

--- assignments/prototypes.js
+++ assignments/prototypes.js
@@ -33,12 +33,14 @@ GameObject.prototype.destroy = function(){
 */
 function CharacterStats(attr){
   GameObject.call(this,attr);
-  this.healthPoints = attr.healthPoints;  
+  this.healthPoints = attr.healthPoints;
+  this.alive = attr.alive;
 };
 CharacterStats.prototype = Object.create(GameObject.prototype); // inherit GameObject methods
 
-CharacterStats.prototype.takeDamage = function() {
-  return `${this.name} took damage`;
+CharacterStats.prototype.takeDamage = function(amount) {
+  this.healthPoints -= amount;
+  return `${this.name} took ${amount} damage`;
 };
 
 
@@ -53,20 +55,50 @@ CharacterStats.prototype.takeDamage = function() {
 */
 
 function Humanoid(attr) {
-  // GameObject.call(this.attr);     // Inherit GameObject properties
   CharacterStats.call(this,attr); // Inherit CharacterStats properties -- needed??
   this.team = attr.team;
   this.weapons = attr.weapons;
   this.language = attr.language;
 };
 
-Humanoid.prototype = Object.create(GameObject.prototype);   // Inherit GameObject properties
+// Humanoid.prototype = Object.create(GameObject.prototype);   // Inherit GameObject properties
 Humanoid.prototype = Object.create(CharacterStats.prototype); // Inherit Humanoid properties
 
 Humanoid.prototype.greet = function() {
   return `${this.name} offers a greeting in ${this.language}`;
 };
- 
+
+// * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function. --done
+// * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
+
+function Villain (attr) {
+  Humanoid.call(this,attr); // Inherit Humanoid properties
+};
+Villain.prototype = Object.create(Humanoid.prototype); // Inherit Humanoid methods
+Villain.prototype.stab_attack = function(target) {
+  let damage = 4;
+  target.takeDamage(damage);
+  if (target.healthPoints <= 0){
+    target.destroy();
+    return `${this.name} did a STAB ATTACK on ${target.name}, and did ${damage} damage! and killed them! SAVAGE`
+  } 
+  return `${this.name} did a STAB ATTACK on ${target.name}, and did ${damage} damage!`;
+};
+
+function Hero (attr) {
+  Humanoid.call(this,attr); // Inherit Humanoid properties
+};
+Hero.prototype = Object.create(Humanoid.prototype); // Inherit Humanoid methods
+Hero.prototype.jump_attack = function (target) {
+  let damage = 5;
+  target.takeDamage(damage);
+  if (target.healthPoints <= 0){
+    target.destroy();
+    return `${this.name} did a JUMP ATTACK on ${target.name}, and did ${damage} damage! and killed them! SAVAGE`
+  } 
+  return `${this.name} did a JUMP ATTACK on ${target.name}, and did ${damage} damage!`;
+};
+
 /*
   * Inheritance chain: GameObject -> CharacterStats -> Humanoid
   * Instances of Humanoid should have all of the same properties as CharacterStats and GameObject.
@@ -83,6 +115,7 @@ Humanoid.prototype.greet = function() {
       height: 1,
     },
     healthPoints: 5,
+    alive: true,
     name: 'Bruce',
     team: 'Mage Guild',
     weapons: [
@@ -99,6 +132,7 @@ Humanoid.prototype.greet = function() {
       height: 2,
     },
     healthPoints: 15,
+    alive: true,
     name: 'Sir Mustachio',
     team: 'The Round Table',
     weapons: [
@@ -116,6 +150,7 @@ Humanoid.prototype.greet = function() {
       height: 4,
     },
     healthPoints: 10,
+    alive: true,
     name: 'Lilith',
     team: 'Forest Kingdom',
     weapons: [
@@ -133,7 +168,7 @@ Humanoid.prototype.greet = function() {
   console.log(mage.weapons); // Staff of Shamalama
   console.log(archer.language); // Elvish
   console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
+  console.log(mage.takeDamage(10)); // Bruce took damage.
   console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
 
 
@@ -141,3 +176,48 @@ Humanoid.prototype.greet = function() {
   // * Create Villain and Hero constructor functions that inherit from the Humanoid constructor function.  
   // * Give the Hero and Villains different methods that could be used to remove health points from objects which could result in destruction if health gets to 0 or drops below 0;
   // * Create two new objects, one a villain and one a hero and fight it out with methods!
+
+  const jon = new Hero({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 8,
+    alive: true,
+    name: 'Jon Snow',
+    team: 'The North',
+    weapons: [
+      'Sword'
+    ],
+    language: 'Human',
+  });
+
+  const nk = new Villain({
+    createdAt: new Date(),
+    dimensions: {
+      length: 1,
+      width: 2,
+      height: 4,
+    },
+    healthPoints: 14,
+    alive: true,
+    name: 'Night King',
+    team: 'Dead',
+    weapons: [
+      'Sword'
+    ],
+    language: 'Zombie',
+  });
+
+  console.log(`******************STRETCH Battle against: ${jon.name} and ${nk.name} *******************`);
+  console.log(`Round 1`);
+  console.log(`JON HP:${jon.healthPoints} | NIGHT KING HP:${nk.healthPoints}`);
+  console.log(jon.jump_attack(nk));
+  console.log(nk.stab_attack(jon));
+  console.log(`Round 2`);
+  console.log(`JON HP:${jon.healthPoints} | NIGHT KING HP:${nk.healthPoints}`);
+  console.log(jon.jump_attack(nk));
+  console.log(nk.stab_attack(jon));
+  
